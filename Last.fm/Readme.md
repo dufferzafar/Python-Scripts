07488ad13e79ae94b39bbd7ae7832f85d1536aab
Shadab Zafar <shadab.softs@gmail.com>
2013-10-20 22:38:08 +0530

    Top Tracks Completed.

--- Last.fm/Readme.md
+++ Last.fm/Readme.md
@@ -1,6 +1,10 @@
-# Last.fm Tracks
+# Last.fm Top Tracks v1.0
 
 Get top tracks of an artist and then locate them on the filesystem.
+
+Usage Scenario:
+
+I mostly download the entire discography of artists. So this script comes in handy to find the 'top' tracks, and build a playlist from it. I can then listen to only those tracks.
 
 ## Table of Contents
 
@@ -12,9 +16,10 @@ Get top tracks of an artist and then locate them on the filesystem.
 
 ## <a name="todo"></a>Todo
 
-* Create playlist
+* Look in metadata too
 
 ## <a name="changelog"></a>Changelog
 
+* M3U Playlist Generated
 * Can find files
 * XML Output Parsed
--- Last.fm/Top20.py
+++ Last.fm/Top20.py
@@ -1,35 +1,67 @@
-# import json
-import urllib.parse
 import os
+import urllib.parse
 import xml.etree.ElementTree as ET
 
 import HelperFunctions
 
-# artist = "Lifehouse"
-# artistFolder = "F:\More Music\Lifehouse"
+# The artist to look for
+artist = "Owl City"
 
-artist = "God is an Astronaut"
-artistFolder = "F:\More Music\God is an Astronaut"
+# Discography Folder
+if os.path.isdir("F:\\More Music\\" + artist):
+    artistFolder = "F:\\More Music\\" + artist
+elif os.path.isdir("G:\\Music\\" + artist):
+    artistFolder = "G:\\Music\\" + artist
+else:
+    print("Where to look for music files?")
+    quit()
+
+# Number of tracks to download
 limit = "20"
 
+# Don't abuse, Please.
 api_key = "da70281f2f464cfaa4638c4bfe820f9a"
-root_url = "http://ws.audioscrobbler.com/2.0/?"
 
+# Last.fm API
+root_url = "http://ws.audioscrobbler.com/2.0/?"
 params = urllib.parse.urlencode({'method': 'artist.gettoptracks', 'artist': artist, 'api_key': api_key, 'limit': limit})
 
-fileName = artist + " - Top " + limit + ".xml"
+# The file where lastfm response will be saved
+fileName = "Artists\\" + artist + " - Top " + limit + ".xml"
+
+print("Downloading File...")
+
+# Download to file
 HelperFunctions.downloadToFile(root_url+params, fileName)
 
+# Build a parse tree
 tree = ET.parse(fileName).find("toptracks")
 
+# Output playlist file
+playlist = artistFolder + "\\" + artist + " - Top " + limit + ".m3u"
+op = open(playlist, "w")
+
+print("Building Playlist...")
+
+# Iter through the XML
 for item in tree.findall("track"):
-    # print(item.find("name").text)
-    # track = "You and Me"
     track = item.find("name").text
+    # print(track)
 
-    print(track, end=" ==> ")
-
+    # Walk in the directory
     for dirpath, dirnames, files in os.walk(artistFolder):
         for filename in files:
             if track.lower() in filename.lower():
-                print(filename)
+                # print(filename)
+                # print(os.path.join(dirpath, filename))
+                op.write(os.path.join(dirpath, filename))
+                op.write("\n")
+                count += 1
+
+# Cleanup
+op.close()
+
+print("All Done!")
+
+# Play Some Music!!
+os.startfile(playlist)
