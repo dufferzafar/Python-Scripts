739f745d6dadf4dc62bfafbd6f76be3a327774bd
Shadab Zafar <shadab.softs@gmail.com>
2013-10-26 22:53:38 +0530

    December Minutes Per Day Graph

--- Networx XML Parser/Networx Parser.py
+++ Networx XML Parser/Networx Parser.py
@@ -2,14 +2,43 @@ from datetime import datetime as DT
 from datetime import timedelta
 import xml.etree.ElementTree as ET
 
-sessStart = DT.strptime("2012-12-01 14:10:31", "%Y-%m-%d %H:%M:%S")
-sessDuration = timedelta(seconds=int("4692"))
+# Current File
+# todo: Rename the files present in Backups Folder
+fileName = "Backups/Dec-2012.xml"
 
-# print(sessStart.date().day, sessStart.time())
-# print(sessStart.time(), (sessStart + sessDuration).time())
+# Build a parse tree
+tree = ET.parse(fileName).find("dialup")
 
-l = [[1,2,3],
-     [4,5,6],
-     [7,8,9]]
+# Per Day
+timePerDay = [0 for i in range(1, 33)]
 
-print(l[87])
+# Rock This Show!!
+for item in tree.findall("item"):
+    conName = item.find("connection").text
+    if conName != "MBlaze USB Modem":
+        continue
+
+    dur = item.find("duration").text
+    since = item.find("since").text
+
+    sessStart = DT.strptime(since, "%Y-%m-%d %H:%M:%S")
+    # sessDuration = timedelta(seconds=int(dur))
+
+    timePerDay[sessStart.date().day] += int(dur)
+
+# Build Output Graphing Lists
+labelValues = [0 for i in range(1, 33)]
+dataValues = [0 for i in range(1, 33)]
+
+# Time Spent Per Day on the Internet
+for i in range(1,32):
+    labelValues[i] = i
+    dataValues[i] = timePerDay[i]//60
+
+    print(str(i) + " " + str(timedelta(seconds=timePerDay[i])))
+
+# Write Output to fileself.
+opFile = open("Output\dataset.js", "w")
+opFile.write("var labelValues = " + str(labelValues))
+opFile.write("\nvar dataValues = " + str(dataValues))
+opFile.close()
--- Networx XML Parser/Readme.md
+++ Networx XML Parser/Readme.md
@@ -1,17 +1,43 @@
-# Networx XML Parser
+# Networx XML Parser v0.2
 
-Parse Networx backup XMLs to create statistics on Internet Usage.
+Parse [Networx](http://www.softperfect.com/products/networx) backup XMLs to create statistics on Internet Usage.
+
 
 ## Table of Contents
 
-<!-- * [Usage Instructions](#usage) -->
+* [Usage Scenario](#usage)
 * [Stuff to do](#todo)
 * [Changelog](#changelog)
 
-<!-- ## <a name="usage"></a>Usage -->
+## <a name="usage"></a>Usage Scenario
+
+I've been creating a backup of Networx Data every month for the past one year. The backups are saved in XML format.
+
+I now wish to use all that data to create some usage statistics.
+
+These set of scripts will parse those XML Files to convert the crude data into a more usable format.
+
+I'll proabably be creating js files so that the data can be charted using some graphing library.
 
 ## <a name="todo"></a>Todo
 
+* Better Graphing Ideas
+
+* Time Per Day
+  * Matrix[Month][Day]
+
+* Sessions Per Day
+  * Started At
+  * Duration
+  * Ended At
+
+* Connection Wise
 
 ## <a name="changelog"></a>Changelog
 
+* December Graph Done :)
+* Time Per Day for December
+
+* Testing/Learning
+  * List = [0] * 10
+  * Datetime Module: strptime, timedelta
