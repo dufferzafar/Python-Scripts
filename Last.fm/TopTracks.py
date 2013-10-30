5f6e010731516e3329de0e4f7fb59aee786b1aca
Shadab Zafar <shadab.softs@gmail.com>
2013-10-30 19:04:04 +0530

    TopTracks: Generate Playlists for Entire Discography.

--- Last.fm/TopTracks.py
+++ Last.fm/TopTracks.py
@@ -11,17 +11,7 @@ import HelperFunctions
 mode = "playlist"
 
 # The artist to look for
-artist = "U2"
-
-# Discography Folder
-if mode == "playlist":
-    if os.path.isdir("F:\\More Music\\" + artist):
-        artistFolder = "F:\\More Music\\" + artist
-    elif os.path.isdir("G:\\Music\\" + artist):
-        artistFolder = "G:\\Music\\" + artist
-    else:
-        print("Where to look for music files?")
-        quit()
+# artist = "U2"
 
 # Number of tracks to download
 limit = "20"
@@ -31,54 +21,66 @@ api_key = "da70281f2f464cfaa4638c4bfe820f9a"
 
 # Last.fm API
 root_url = "http://ws.audioscrobbler.com/2.0/?"
-params = urllib.parse.urlencode({'method': 'artist.gettoptracks', 'artist': artist, 'api_key': api_key, 'limit': limit})
 
-# The file where lastfm response will be saved
-fileName = "Artists\\" + artist + " - Top " + limit + ".xml"
+for artist in os.listdir("G:\\Music"):
+    if '[' not in artist:
 
-print("Downloading File...")
+        # Discography Folder
+        if mode == "playlist":
+            if os.path.isdir("F:\\More Music\\" + artist):
+                artistFolder = "F:\\More Music\\" + artist
+            elif os.path.isdir("G:\\Music\\" + artist):
+                artistFolder = "G:\\Music\\" + artist
+            else:
+                print("Where to look for music files?")
+                quit()
 
-# Download to file
-HelperFunctions.downloadToFile(root_url+params, fileName)
+        # The file where lastfm response will be saved
+        fileName = "Artists\\" + artist + " - Top " + limit + ".xml"
 
-# Build a parse tree
-tree = ET.parse(fileName).find("toptracks")
-# Decide what to do
-if mode == "list":
-    # os.system('clear')
-    # subprocess.call("cls", shell=True) # windows
+        print("Downloading File... " + artist)
 
-    for item in tree.findall("track"):
-        track = item.find("name").text
-        playcount = item.find("playcount").text
-        listeners = item.find("listeners").text
-        print(track, playcount, listeners)
+        # Download to file
+        params = urllib.parse.urlencode({'method': 'artist.gettoptracks', 'artist': artist, 'api_key': api_key, 'limit': limit})
+        HelperFunctions.downloadToFile(root_url+params, fileName)
 
-elif mode == "playlist":
-    # Output playlist file
-    playlist = artistFolder + "\\" + artist + " - Top " + limit + ".m3u"
-    op = open(playlist, "w")
+        # Build a parse tree
+        tree = ET.parse(fileName).find("toptracks")
+        # Decide what to do
+        if mode == "list":
+            # os.system('clear')
+            # subprocess.call("cls", shell=True) # windows
 
-    print("Building Playlist...")
+            for item in tree.findall("track"):
+                track = item.find("name").text
+                playcount = item.find("playcount").text
+                listeners = item.find("listeners").text
+                print(track, playcount, listeners)
 
-    # Iter through the XML
-    for item in tree.findall("track"):
-        track = item.find("name").text
-        # print(track)
+        elif mode == "playlist":
+            # Output playlist file
+            playlist = artistFolder + "\\" + artist + " - Top " + limit + ".m3u"
+            op = open(playlist, "w")
 
-        # Walk in the directory
-        for dirpath, dirnames, files in os.walk(artistFolder):
-            for filename in files:
-                if track.lower() in filename.lower():
-                    # print(filename)
-                    # print(os.path.join(dirpath, filename))
-                    op.write(os.path.join(dirpath, filename))
-                    op.write("\n")
+            print("Building Playlist... " + artist)
 
-    # Cleanup
-    op.close()
+            # Iter through the XML
+            for item in tree.findall("track"):
+                track = item.find("name").text
 
-    print("All Done!")
+                # Walk in the directory
+                for dirpath, dirnames, files in os.walk(artistFolder):
+                    for filename in files:
+                        if track.lower() in filename.lower():
+                            op.write(os.path.join(dirpath, filename))
+                            op.write("\n")
 
-    # Play Some Music!!
-    os.startfile(playlist)
+            # Cleanup
+            op.close()
+
+            print("=====================")
+
+            # Play Some Music!!
+            # os.startfile(playlist)
+# Whew!!
+print("All Done!")
