6c29547889b8298d4747302f376965a6090a042a
Shadab Zafar <shadab.softs@gmail.com>
2013-10-31 18:36:47 +0530

    TopTracks: Back to Single Artist Version

--- Last.fm/ScrobblesToday.py
+++ Last.fm/ScrobblesToday.py
@@ -1,24 +1,24 @@
 from datetime import datetime as DT
+from datetime import timedelta
 import time
 import urllib.request
 import urllib.parse
 import xml.etree.ElementTree as ET
 
 # from datetime import timedelta
-# http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=dufferzafar&from=1383004800&api_key=da70281f2f464cfaa4638c4bfe820f9a
 # http://ws.audioscrobbler.com/2.0/?method=user.getInfo&user=dufferzafar&api_key=da70281f2f464cfaa4638c4bfe820f9a
 # http://ws.audioscrobbler.com/2.0/?method=user.getFriends&user=dufferzafar&recenttracks=0&api_key=da70281f2f464cfaa4638c4bfe820f9a
 
 # The timestamp of today
-unixTSToday = int(time.mktime(time.strptime(DT.strftime(DT.today(), '%Y-%m-%d'), '%Y-%m-%d'))) - time.timezone
+unixTimeStamp = int(time.mktime(time.strptime(DT.strftime(DT.today() - timedelta(days=0), '%Y-%m-%d'), '%Y-%m-%d'))) - time.timezone
 
 # Don't abuse, Please.
 api_key = "da70281f2f464cfaa4638c4bfe820f9a"
 
 # Last.fm API
 root_url = "http://ws.audioscrobbler.com/2.0/?"
-params = urllib.parse.urlencode({'method': 'user.getRecentTracks', 'user': 'dufferzafar', 'from': unixTSToday, 'api_key': api_key})
-params2 = urllib.parse.urlencode({'method': 'user.getRecentTracks', 'user': 'shivamrana', 'from': unixTSToday, 'api_key': api_key})
+params = urllib.parse.urlencode({'method': 'user.getRecentTracks', 'user': 'dufferzafar', 'from': unixTimeStamp, 'api_key': api_key})
+params2 = urllib.parse.urlencode({'method': 'user.getRecentTracks', 'user': 'shivamrana', 'from': unixTimeStamp, 'api_key': api_key})
 # The file where lastfm response will be saved
 # print("Downloading Data...")
 
--- Last.fm/TopTracks.py
+++ Last.fm/TopTracks.py
@@ -11,7 +11,7 @@ import HelperFunctions
 mode = "playlist"
 
 # The artist to look for
-# artist = "U2"
+artist = "U2"
 
 # Number of tracks to download
 limit = "20"
@@ -22,65 +22,63 @@ api_key = "da70281f2f464cfaa4638c4bfe820f9a"
 # Last.fm API
 root_url = "http://ws.audioscrobbler.com/2.0/?"
 
-for artist in os.listdir("G:\\Music"):
-    if '[' not in artist:
+# Discography Folder
+if mode == "playlist":
+    if os.path.isdir("F:\\More Music\\" + artist):
+        artistFolder = "F:\\More Music\\" + artist
+    elif os.path.isdir("G:\\Music\\" + artist):
+        artistFolder = "G:\\Music\\" + artist
+    else:
+        print("Where to look for music files?")
+        quit()
 
-        # Discography Folder
-        if mode == "playlist":
-            if os.path.isdir("F:\\More Music\\" + artist):
-                artistFolder = "F:\\More Music\\" + artist
-            elif os.path.isdir("G:\\Music\\" + artist):
-                artistFolder = "G:\\Music\\" + artist
-            else:
-                print("Where to look for music files?")
-                quit()
+# The file where lastfm response will be saved
+fileName = "Artists\\" + artist + " - Top " + limit + ".xml"
 
-        # The file where lastfm response will be saved
-        fileName = "Artists\\" + artist + " - Top " + limit + ".xml"
+print("Downloading File... " + artist)
 
-        print("Downloading File... " + artist)
+# Download to file
+params = urllib.parse.urlencode({'method': 'artist.gettoptracks', 'artist': artist, 'api_key': api_key, 'limit': limit})
+HelperFunctions.downloadToFile(root_url+params, fileName)
 
-        # Download to file
-        params = urllib.parse.urlencode({'method': 'artist.gettoptracks', 'artist': artist, 'api_key': api_key, 'limit': limit})
-        HelperFunctions.downloadToFile(root_url+params, fileName)
+# Build a parse tree
+tree = ET.parse(fileName).find("toptracks")
+# Decide what to do
+if mode == "list":
+    # os.system('clear')
+    # subprocess.call("cls", shell=True) # windows
 
-        # Build a parse tree
-        tree = ET.parse(fileName).find("toptracks")
-        # Decide what to do
-        if mode == "list":
-            # os.system('clear')
-            # subprocess.call("cls", shell=True) # windows
+    for item in tree.findall("track"):
+        track = item.find("name").text
+        playcount = item.find("playcount").text
+        listeners = item.find("listeners").text
+        print(track, playcount, listeners)
 
-            for item in tree.findall("track"):
-                track = item.find("name").text
-                playcount = item.find("playcount").text
-                listeners = item.find("listeners").text
-                print(track, playcount, listeners)
+elif mode == "playlist":
+    # Output playlist file
+    playlist = artistFolder + "\\" + artist + " - Top " + limit + ".m3u"
+    op = open(playlist, "w")
 
-        elif mode == "playlist":
-            # Output playlist file
-            playlist = artistFolder + "\\" + artist + " - Top " + limit + ".m3u"
-            op = open(playlist, "w")
+    print("Building Playlist... " + artist)
 
-            print("Building Playlist... " + artist)
+    # Iter through the XML
+    for item in tree.findall("track"):
+        track = item.find("name").text
 
-            # Iter through the XML
-            for item in tree.findall("track"):
-                track = item.find("name").text
+        # Walk in the directory
+        for dirpath, dirnames, files in os.walk(artistFolder):
+            for filename in files:
+                if track.lower() in filename.lower():
+                    op.write(os.path.join(dirpath, filename))
+                    op.write("\n")
 
-                # Walk in the directory
-                for dirpath, dirnames, files in os.walk(artistFolder):
-                    for filename in files:
-                        if track.lower() in filename.lower():
-                            op.write(os.path.join(dirpath, filename))
-                            op.write("\n")
+    # Cleanup
+    op.close()
 
-            # Cleanup
-            op.close()
+    print("=====================")
 
-            print("=====================")
+    # Play Some Music!!
+    os.startfile(playlist)
 
-            # Play Some Music!!
-            # os.startfile(playlist)
 # Whew!!
-print("All Done!")
+# print("All Done!")
