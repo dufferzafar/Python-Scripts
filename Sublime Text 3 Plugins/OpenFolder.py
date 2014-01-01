f2bfd140ed8f33a846b36f0fb1b8fa16224a695b
Shadab Zafar <dufferzafar0@gmail.com>
2014-01-02 01:53:25 +0530

    Updated Open Folders

--- Sublime Text 3 Plugins/OpenFolder.py
+++ Sublime Text 3 Plugins/OpenFolder.py
@@ -2,14 +2,12 @@ import os
 import sublime, sublime_plugin
 from subprocess import call
 
-class openProjectFolderCommand(sublime_plugin.WindowCommand):
-    def run( self ):
-
-        call([ "explorer", self.window.folders()[0] ])
-
-class openFileFolderCommand( sublime_plugin.WindowCommand ):
+class openFolderCommand( sublime_plugin.WindowCommand ):
   def run( self ):
     if self.window.active_view() is None:
       return
 
-    call([ "explorer", os.path.dirname(self.window.active_view().file_name()) ])
+    try:
+      call([ "explorer", self.window.folders()[0] ])
+    except:
+      call([ "explorer", os.path.dirname(self.window.active_view().file_name()) ])
