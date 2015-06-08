448dc52ebc7e567be10eba6116ceefd76fb87a18
Shadab Zafar <dufferzafar0@gmail.com>
2015-06-08 15:57:48 +0530

    Github Contributions: Parse HTML files to extract contributions

--- Github Contributions/contribs.py
+++ Github Contributions/contribs.py
@@ -45,3 +45,64 @@ def fetch_pages(years):
                 params = (user, year, month, year, month+1)
 
             process_url(gh_url % params, out)
+
+
+def list_contributions(years):
+    """
+    List all repositories to which contributions have been made.
+
+    You can then fetch commits off those repos.
+    """
+
+    repos = []
+    issues = []
+    pulls = []
+
+    def process_file(file_):
+        with open(file_) as inp:
+            soup = BeautifulSoup(inp.read())
+
+        for link in soup.find_all("a", class_="title"):
+
+            url = link.get('href')
+            if not url:
+                continue
+
+            m = re.match(r'/?(.*?)/(.*?)/commits', url)
+            if m:
+                repo = (m.group(1), m.group(2))
+                if repo not in repos:
+                    repos.append(repo)
+                continue
+
+            m = re.match(r'/?(.*?)/(.*?)/issues/(\d+)', url)
+            if m:
+                issue = (m.group(1), m.group(2), m.group(3))
+                if issue not in issues:
+                    issues.append(issue)
+                continue
+
+            m = re.match(r'/?(.*?)/(.*?)/pull/(\d+)', url)
+            if m:
+                pull = (m.group(1), m.group(2), m.group(3))
+                if pull not in pulls:
+                    pulls.append(pull)
+                continue
+
+            # We should never reach here!
+            print()
+            print("<|>" + link.parent.text.strip() + "<|>")
+            print(url)
+            print()
+
+    for year in years:
+        for month in range(1, 13):
+            process_file(os.path.join(data, str(year), str(month)) + ".htm")
+
+    return repos, issues, pulls
+
+if __name__ == '__main__':
+    # fetch_pages(range(2015, 2016))
+    r, i, p = list_contributions([2014])
+
+    print(r)
